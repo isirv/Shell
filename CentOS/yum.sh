bd7c0716a6c0af932f3ef07957575785bb3243ff
tiantao <tiantao@shinc.net>
2015-07-01 14:36:17 +0800

    modify yum.sh

--- CentOS/yum.sh
+++ CentOS/yum.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
  
 echo 'select mirrors:'
-mirrors_163='1. 163'
-mirrors_sohu='2. sohu'
+mirrors_163='1. 163.'
+mirrors_sohu='2. sohu.'
+mirrors_other='3. Exit.'
+
 url_163='http://mirrors.163.com/.help/CentOS6-Base-163.repo'
 url_sohu='http://mirrors.sohu.com/help/CentOS-Base-sohu.repo'
 echo  $mirrors_163
 echo  $mirrors_sohu
+echo  $mirrors_other
  
 read select
  
-if [ 2 != $select ]
+if [ 1 == $select ]
 then
         url=$url_163
         fileName='CentOS6-Base-163.repo'
-else
+elif [ 2 == $select ]
+then
         url=$url_sohu
         fileName='CentOS-Base-sohu.repo'
+else
+	exit
 fi
+
+mkdir -p /etc/yum.repos.d/yum.bak
+mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/yum.bak/
  
 wget $url
  
@@ -29,8 +38,6 @@ else
         exit
 fi
  
-mkdir -p /etc/yum.repos.d/yum.bak
-mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/yum.bak/
 mv ${fileName} /etc/yum.repos.d/CentOS-Base.repo
  
 yum clean all
