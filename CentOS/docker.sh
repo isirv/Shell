04651b55ce8692c99cf094c5a1423584410e6650
tiantao <tiantao@shinc.net>
2015-07-02 12:35:42 +0800

    docker.sh

--- CentOS/docker.sh
+++ CentOS/docker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# install docker
+# install epel
+wget http://mirror.centos.org/centos/7/extras/x86_64/Packages/epel-release-7-5.noarch.rpm
 
+rpm -ivh epel-release-7-5.noarch.rpm
+
+# install docker
+wget http://www.hop5.in/yum/el6/hop5.repo
+mv hop5.repo /etc/yum.repos.d/
 yum -y install docker-io
 service docker start
 chkconfig docker on
