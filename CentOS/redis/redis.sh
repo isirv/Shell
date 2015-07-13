b0741485f7672dfcb10e117b2b37af791c0d75f2
tiantao <tiantao@shinc.net>
2015-07-13 15:57:05 +0800

    redis install

--- CentOS/redis/redis.sh
+++ CentOS/redis/redis.sh
@@ -52,5 +52,17 @@ cd /usr/local/
 mv redis-3.0.2 redis && cd redis
 make & make test & make install
 
-echo "server: src/redis-server"
-echo "client: src/redis-cli"
+# 将src下的可执行命令全部移动到/usr/local/redis/bin/目录下
+mkdir bin & mkdir etc
+cd ./src
+mv mkreleasehdr.sh redis-benchmark redis-check-aof redis-check-dump redis-cli redis-sentinel redis-server /usr/local/redis/bin
+
+# 修改配置文件路径
+cd ..
+mv redis.conf /usr/local/redis/etc/
+
+
+
+echo "启动redis server命令: cd /usr/local/redis/bin/ & ./redis-server /usr/local/redis/etc/redis.conf"
+echo "注意：开启redis服务需要指定配置文件，如不指定配置文件则加载默认配置文件。"
+echo "启动redis client命令: cd /usr/local/redis/bin/ & ./redis-cli /usr/local/redis/etc/redis.conf"
